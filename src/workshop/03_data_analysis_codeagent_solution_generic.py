ad14b2c71e6f65eea251583bcacc98eafc250b47
William Droz <william.droz@idiap.ch>
2026-02-15 20:08:33 +0100

    feat: generic solution for 03

---
+++ src/workshop/03_data_analysis_codeagent_solution_generic.py
@@ -0,0 +1,27 @@
+"""
+How to make your first agent with pydantic - solution
+"""
+
+from pathlib import Path
+from dotenv import load_dotenv
+from smolagents import CodeAgent
+from workshop.common import smolagents_build_model
+
+load_dotenv()
+
+
+def build_agent():
+    model = smolagents_build_model()
+    agent = CodeAgent(
+        model=model, tools=[], additional_authorized_imports=["polars, pandas"]
+    )
+    return agent
+
+
+if __name__ == "__main__":
+    agent = build_agent()
+    user_question = input("Ask the agent: ")
+    dataset_path = Path(__file__).parent.parent.parent / "dataset" / "voting.csv"
+    prompt = f"You are a data analyst, you have the following dataset:\n{dataset_path}\nPlease use polars to answer the user's question: {user_question}"
+    result = agent.run(prompt)
+    print(result)
